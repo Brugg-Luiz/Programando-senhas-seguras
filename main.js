0550d980d0630cfe206b35fb95e8435d786d3953
Brugg-Luiz <brugg.luiz@escola.pr.gov.br>
2026-07-09 11:52:36 -0300

    ,

--- main.js
+++ main.js
@@ -28,3 +28,8 @@ function aumentaTamanho(){
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
+for (i = 0; i < checkbox.length; i++) {
+    checkbox[i].onclick = geraSenha;
+}
+
+geraSenha();
